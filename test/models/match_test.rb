a8cbe761d28e6efd72f81f1df71a76c7452a0faa
Al <alberto@Als-MacBook-Pro.local>
2018-01-02 12:42:51 -0600

    tests

--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class MatchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "should_not_save_empty_match" do
+     	match = Match.new
+  		assert_not match.save
+   end
+
+   
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,23 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should_calculate_correct_score" do
+     	user = User.new
+     	user.email = rand( 10000).to_s + "a@aaa.com"
+     	user.password ="secret123"
+     	user.save
+
+     	match = Match.new
+     	match.owner = user
+     	match.their_score = 30
+     	match.your_score  = 1
+     	match.played_date = Date.new()
+     	match.opponent = User.all.last
+
+     	match.save
+
+  		assert user.score == 50
+   end
+
 end
